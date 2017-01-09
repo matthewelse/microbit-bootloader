16f124b9cbd53a2e8fc19430c02e49df32b4b9ed
Matthew Else <matthewelse1997@gmail.com>
2017-01-09 21:35:25 +0000

    ignore init packet entirely

--- components/libraries/bootloader_dfu/dfu_init_template.c
+++ components/libraries/bootloader_dfu/dfu_init_template.c
@@ -56,99 +56,31 @@ static uint8_t m_extended_packet_length;                            //< Length o
 
 uint32_t dfu_init_prevalidate(uint8_t * p_init_data, uint32_t init_data_len)
 {
-    uint32_t i = 0;
-    
-    // In order to support signing or encryption then any init packet decryption function / library
-    // should be called from here or implemented at this location.
-
-    // Length check to ensure valid data are parsed.
-    if (init_data_len < sizeof(dfu_init_packet_t))
-    {
-        return NRF_ERROR_INVALID_LENGTH;
-    }
-
-    // Current template uses clear text data so they can be casted for pre-check.
-    dfu_init_packet_t * p_init_packet = (dfu_init_packet_t *)p_init_data;
-
-    m_extended_packet_length = ((uint32_t)p_init_data + init_data_len) -
-                               (uint32_t)&p_init_packet->softdevice[p_init_packet->softdevice_len];
-    if (m_extended_packet_length < DFU_INIT_PACKET_EXT_LENGTH_MIN)
-    {
-        return NRF_ERROR_INVALID_LENGTH;
-    }
-
-    if (((uint32_t)p_init_data + init_data_len) < 
-        (uint32_t)&p_init_packet->softdevice[p_init_packet->softdevice_len])
-    {
-        return NRF_ERROR_INVALID_LENGTH;
-    }
-
-    memcpy(m_extended_packet,
-           &p_init_packet->softdevice[p_init_packet->softdevice_len],
-           m_extended_packet_length);
-
-/** [DFU init application version] */
-    // To support application versioning, this check should be updated.
-    // This template allows for any application to be installed. However, 
-    // customers can place a revision number at the bottom of the application 
-    // to be verified by the bootloader. This can be done at a location 
-    // relative to the application, for example the application start 
-    // address + 0x0100.
-/** [DFU init application version] */
-    
-    // First check to verify the image to be transfered matches the device type.
-    // If no Device type is present in DFU_DEVICE_INFO then any image will be accepted.
-    if ((DFU_DEVICE_INFO->device_type != DFU_DEVICE_TYPE_EMPTY) &&
-        (p_init_packet->device_type != DFU_DEVICE_INFO->device_type))
-    {
-        return NRF_ERROR_INVALID_DATA;
-    }
-    
-    // Second check to verify the image to be transfered matches the device revision.
-    // If no Device revision is present in DFU_DEVICE_INFO then any image will be accepted.
-    if ((DFU_DEVICE_INFO->device_rev != DFU_DEVICE_REVISION_EMPTY) &&
-        (p_init_packet->device_rev != DFU_DEVICE_INFO->device_rev))
-    {
-        return NRF_ERROR_INVALID_DATA;
-    }
-
-    // Third check: Check the array of supported SoftDevices by this application.
-    //              If the installed SoftDevice does not match any SoftDevice in the list then an
-    //              error is returned.
-    while (i < p_init_packet->softdevice_len)
-    {
-        if (p_init_packet->softdevice[i] == DFU_SOFTDEVICE_ANY ||
-            p_init_packet->softdevice[i++] == SOFTDEVICE_INFORMATION->firmware_id)
-        {
-            return NRF_SUCCESS;
-        }
-    }
-    
-    // No matching SoftDevice found - Return NRF_ERROR_INVALID_DATA.
-    return NRF_ERROR_INVALID_DATA;
+    // ignore the init packet :)
+    return NRF_SUCCESS;
 }
 
 
 uint32_t dfu_init_postvalidate(uint8_t * p_image, uint32_t image_len)
 {
-    uint16_t image_crc;
-    uint16_t received_crc;
+    // uint16_t image_crc;
+    // uint16_t received_crc;
     
-    // In order to support hashing (and signing) then the (decrypted) hash should be fetched and
-    // the corresponding hash should be calculated over the image at this location.
-    // If hashing (or signing) is added to the system then the CRC validation should be removed.
+    // // In order to support hashing (and signing) then the (decrypted) hash should be fetched and
+    // // the corresponding hash should be calculated over the image at this location.
+    // // If hashing (or signing) is added to the system then the CRC validation should be removed.
 
-    // calculate CRC from active block.
-    image_crc = crc16_compute(p_image, image_len, NULL);
+    // // calculate CRC from active block.
+    // image_crc = crc16_compute(p_image, image_len, NULL);
 
-    // Decode the received CRC from extended data.    
-    received_crc = uint16_decode((uint8_t *)&m_extended_packet[0]);
+    // // Decode the received CRC from extended data.    
+    // received_crc = uint16_decode((uint8_t *)&m_extended_packet[0]);
 
-    // Compare the received and calculated CRC.
-    if (image_crc != received_crc)
-    {
-        return NRF_ERROR_INVALID_DATA;
-    }
+    // // Compare the received and calculated CRC.
+    // if (image_crc != received_crc)
+    // {
+    //     return NRF_ERROR_INVALID_DATA;
+    // }
 
     return NRF_SUCCESS;
 }
